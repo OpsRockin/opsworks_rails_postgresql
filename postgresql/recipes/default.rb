74c2470e8e3cd4c367b31a09b323f3530b62fa96
sawanoboly <sawanoboriyu@higanworks.com>
2013-07-03 18:09:24 +0900

    remove not_if for test

--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -14,16 +14,16 @@ node[:deploy].each do |application, deploy|
   execute "create new role for #{application}" do
     action :run
     command %Q{#{psql_command} -c "create user #{application} with password '#{node[:postgresql][:password]}';"}
-    not_if do
-      `#{psql_command} -t -c 'select rolname from pg_roles;'`.split.include?(application)
-    end
+#    not_if do
+#      `#{psql_command} -t -c 'select rolname from pg_roles;'`.split.include?(application)
+#    end
   end
 
   execute "create new database owned by #{application} for #{application}" do
     action :run
     command %Q{#{psql_command} -c "create database #{application}_#{deploy[:rails_env]} with password '#{node[:postgresql][:password]}';"}
-    not_if do
-      `#{psql_command} -t -c 'select datname from pg_database;'`.split.include?("#{application}_#{deploy[:rails_env]}")
-    end
+#    not_if do
+#      `#{psql_command} -t -c 'select datname from pg_database;'`.split.include?("#{application}_#{deploy[:rails_env]}")
+#    end
   end
 end
