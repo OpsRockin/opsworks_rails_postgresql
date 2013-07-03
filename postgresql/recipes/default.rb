031a8512e5e17e78bac174cdeb911c65293ee70b
sawanoboly <sawanoboriyu@higanworks.com>
2013-07-03 18:18:30 +0900

    include_recipe deploy

--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright 2013, HiganWorks LLC
 #
+include_recipe 'deploy'
 node.set[:postgresql] = Mash.new unless node[:postgresql]
 
 node.set[:postgresql][:password] = [OpenSSL::Random.random_bytes(24)].pack("m").chomp unless node[:postgresql][:password]
@@ -14,16 +15,16 @@ node[:deploy].each do |application, deploy|
   execute "create new role for #{application}" do
     action :run
     command %Q{#{psql_command} -c "create user #{application} with password '#{node[:postgresql][:password]}';"}
-#    not_if do
-#      `#{psql_command} -t -c 'select rolname from pg_roles;'`.split.include?(application)
-#    end
+    not_if do
+      `#{psql_command} -t -c 'select rolname from pg_roles;'`.split.include?(application)
+    end
   end
 
   execute "create new database owned by #{application} for #{application}" do
     action :run
     command %Q{#{psql_command} -c "create database #{application}_#{deploy[:rails_env]} with password '#{node[:postgresql][:password]}';"}
-#    not_if do
-#      `#{psql_command} -t -c 'select datname from pg_database;'`.split.include?("#{application}_#{deploy[:rails_env]}")
-#    end
+    not_if do
+      `#{psql_command} -t -c 'select datname from pg_database;'`.split.include?("#{application}_#{deploy[:rails_env]}")
+    end
   end
 end
--- postgresql/recipes/deploy.rb
+++ postgresql/recipes/deploy.rb
@@ -5,6 +5,7 @@
 # Copyright 2013, HiganWorks LLC
 #
 
+include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
   node.set[:deploy][application][:database] = {
     :adapter => 'postgresql',
